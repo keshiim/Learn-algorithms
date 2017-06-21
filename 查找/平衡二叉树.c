e4682252ea648d3303497f30b420f9590f941455
zhengmingjun <keshiim@163.com>
2017-06-21 17:05:51 +0800

    AVL树的插入
    AVL树的插入

--- 查找/平衡二叉树.c
+++ 查找/平衡二叉树.c
@@ -7,6 +7,12 @@
 //
 
 #include "平衡二叉树.h"
+#include <stdlib.h>
+
+typedef enum {
+    FALSE,
+    TRUE
+} Status;
 
 // 二叉树的二叉列表几点结构定义
 // 结点结构
@@ -137,6 +143,107 @@ void RightBalance(BiTree *T) {
             break;
     }
 }
+
+
+/* 若在平衡的二叉排序树T中不存在和e有相同关键字的结点，则插入一个，
+   数据元素为e的新节点并返回1，否则返回0，若因插入而使二叉排序树
+   市区平衡，则做平衡旋转处理，布尔变量taller反映T长高与否 */
+Status InsertAVL(BiTree *T, int e, Status *taller) {
+    if (!*T) {
+        //插入新节点，树“长高”，置taller为TURE
+        *T = (BiTree)malloc(sizeof(BiTNode));
+        (*T)->data = e;
+        (*T)->lchild = (*T)->rchild = NULL;
+        (*T)->bf = EH;
+        *taller = TRUE;
+    }
+    else {
+        if (e == (*T)->data) {
+            //树中已存在和e相同关键字的结点则不再插入
+            *taller = FALSE;
+            return FALSE;
+        }
+        
+        if (e < (*T)->data) {
+            //应继续在T的左子树中进行搜搜
+            //未插入
+            if (InsertAVL(&(*T)->lchild, e, taller) == FALSE) {
+                return FALSE;
+            }
+            
+            //已插入到T的左子树中且左子树“长高”
+            if (*taller) {
+                //检查T的平衡度
+                switch ((*T)->bf) {
+                        //原本左子树比右子树高，需要作左平衡处理
+                    case LH:
+                        LeftBalance(T);
+                        *taller = FALSE;
+                        break;
+                        //原本左右子树等高，先因左子树增高而树增高
+                    case EH:
+                        (*T)->bf = LH;
+                        *taller = TRUE;
+                        break;
+                        //原本右子树比左子树搞，现左右子树等高
+                    case RH:
+                        (*T)->bf = EH;
+                        *taller = FALSE;
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+        else {
+            // 应继续在T的右子树中进行搜索
+            //未插入
+            if (InsertAVL(&(*T)->rchild, e, taller) == FALSE) {
+                return FALSE;
+            }
+            
+            //已插入到T的右子树且右子树“长高”
+            if (*taller) {
+                //检查T的平衡度
+                switch ((*T)->bf) {
+                        //原本左子树比右子树高,现左右子树等高
+                    case LH:
+                        (*T)->bf = EH;
+                        *taller = FALSE;
+                        break;
+                        //原本左右子树等高,现因右子树增高而树增高
+                    case EH:
+                        (*T)->bf = RH;
+                        *taller = TRUE;
+                        break;
+                        //原本右子树比左子树高，需要做右平衡处理
+                    case RH:
+                        RightBalance(T);
+                        *taller = FALSE;
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+    return TRUE;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
