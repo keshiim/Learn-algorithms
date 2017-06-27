bf7c9333994db484d5a2d22ac5999cdbfafa0a7f
zhengmingjun <keshiim@163.com>
2017-06-27 19:36:22 +0800

    优化快排算法的partition

--- 排序/QuickSort.c
+++ 排序/QuickSort.c
@@ -34,6 +34,88 @@ int Partition(SqList *L, int low, int high) {
     return low;
 }
 
+/* 优化：交换顺序表L中子表的记录，是枢轴记录到位，并返回其所在的位置
+ 此时在它之前（后）的记录均不大（小）于它。*/
+int Partition1(SqList *L, int low, int high) {
+    int pivotkey;
+    
+    int m = low + (high - low) / 2;
+    if (L->r[low] > L->r[high]) {
+        //交换左端与右端数据，保证左端较小
+        swap(L, low, high);
+    }
+    if (L->r[m] > L->r[high]) {
+        //交换中间与右端数据，保证中间较小
+        swap(L, m, high);
+    }
+    if (L->r[m] > L->r[low]) {
+        //交换中间与左端数据，保证中间较小
+        swap(L, m, low);
+    }
+    
+    //用子表的第一个记录作枢轴记录
+    pivotkey = L->r[low];
+    //从表的两端交替向中间扫描
+    while (low < high) {
+        while (low < high && L->r[high] >= pivotkey) {
+            high--;
+        }
+        //将比枢轴记录小的记录交换到低端
+        swap(L, low, high);
+        
+        while (low < high && L->r[low] <= pivotkey) {
+            low++;
+        }
+        //将比枢轴记录大的记录交换到高端
+        swap(L, low, high);
+    }
+    
+    //返回枢轴所在的位置
+    return low;
+}
+/* 1的基础上优化：交换顺序表L中子表的记录，是枢轴记录到位，并返回其所在的位置
+此时在它之前（后）的记录均不大（小）于它。*/
+int Partition2(SqList *L, int low, int high) {
+    int pivotkey;
+    
+    int m = low + (high - low) / 2;
+    if (L->r[low] > L->r[high]) {
+        //交换左端与右端数据，保证左端较小
+        swap(L, low, high);
+    }
+    if (L->r[m] > L->r[high]) {
+        //交换中间与右端数据，保证中间较小
+        swap(L, m, high);
+    }
+    if (L->r[m] > L->r[low]) {
+        //交换中间与左端数据，保证中间较小
+        swap(L, m, low);
+    }
+    
+    //用于子表的第一个记录作枢轴记录
+    pivotkey = L->r[low];
+    //将枢轴关键字备份到L->r[0]
+    L->r[0] = pivotkey;
+    //从表的两端交替向中间扫描
+    while (low < high) {
+        while (low < high && L->r[high] >= pivotkey) {
+            high--;
+        }
+        //采用替换而不是交换的方式进行操作
+        L->r[low] = L->r[high];
+        
+        while (low < high && L->r[low] <= pivotkey) {
+            low++;
+        }
+        //采用替换而不是交换的方式操作
+        L->r[high] = L->r[low];
+    }
+    //将枢轴的值替换会L->r[low]
+    L->r[low] = L->r[0];
+    //返回枢轴所在的位置
+    return low;
+}
+
 /* 对顺序表L中的子序列L->r[low..high]作快速排序 */
 void QSort(SqList *L, int low, int high) {
     int pivot;
