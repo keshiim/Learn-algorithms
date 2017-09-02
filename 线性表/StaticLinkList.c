98f3413e1ce7f18c962b86c597e43196397d6a5b
zhengmingjun <keshiim@163.com>
2017-09-02 14:41:17 +0800

    静态链表的malloc

--- 线性表/StaticLinkList.c
+++ 线性表/StaticLinkList.c
@@ -8,7 +8,7 @@
 
 #include "StaticLinkList.h"
 
-Status InitList_SL(StaticLinkList space) {
+Status InitList_SLL(StaticLinkList space) {
     
     for (int i = 0; i < MAXSIZE_STATIC; i++) {
         space[i].cur = i + 1;
@@ -16,4 +16,46 @@ Status InitList_SL(StaticLinkList space) {
     //目前静态链表为空，最后一个元素的cur为0
     space[MAXSIZE_STATIC - 1].cur = 0;
     return OK;
+}
+
+
+int Malloc_SLL(StaticLinkList space) {
+    //获取静态链表下一个备用链表的第一个元素下标
+    int i = space[0].cur;
+    //如果存在，将下一个备用链表的分量做备用
+    if (space[0].cur) {
+        space[0].cur = space[i].cur;
+    }
+    return i;
+}
+
+int ListLength_SSL(StaticLinkList L) {
+    int length = 0;
+    int k = L[MAXSIZE_STATIC - 1].cur;
+    while (k) {
+        length++;
+        k = L[k].cur;
+    }
+    return length;
+}
+
+Status ListInsert_SSL(StaticLinkList L, int i, ElemType e) {
+    int k, j, l;
+    //首先将k指向头结点，即：下标为MAXSIZE_STATIC - 1的位置
+    k = MAXSIZE_STATIC - 1;
+    if (i < 1 || i > ListLength_SSL(L) + 1) {
+        return ERROR;
+    }
+    j = Malloc_SLL(L);//获得空闲分量的下标
+    if (j) {
+        L[j].data = e;
+        //找到i之前的下标位置
+        for (l = 1; l <= i - 1; l++) {
+            k = L[k].cur;
+        }
+        //插入点j
+        L[j].cur = L[k].cur;
+        L[k].cur = j;
+    }
+    return ERROR;
 }
--- 线性表/StaticLinkList.h
+++ 线性表/StaticLinkList.h
@@ -16,7 +16,9 @@
 /*
    静态链表，每个元素E由两个元素组成，数据域：data，
    链表指针域cur(游标：用来描述下一个节点的位置)。
- 
+两个特殊元素：
+   第一个是下标为0的元素，用于存放备用链表的第一个结点的下标。
+   最后一个是末尾下标元素，用于存放第一个有数值的元素下标。链表为空默认是0
  */
 typedef struct {
     ElemType data;
@@ -24,6 +26,15 @@ typedef struct {
     int cur;
 } Component, StaticLinkList[MAXSIZE_STATIC];
 
-Status InitList_SL(StaticLinkList space);
+Status InitList_SLL(StaticLinkList space);
+
+//分配一个空间空间，返回空闲下标，如果备用链表为空则返回0
+int Malloc_SLL(StaticLinkList space);
+
+//静态链表的长度
+int ListLength_SSL(StaticLinkList L);
+
+//在链表中第i个元素之前插入e
+Status ListInsert_SSL(StaticLinkList L, int i, ElemType e);
 
 #endif /* StaticLinkList_h */
